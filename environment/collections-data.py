8234cb39f020ef459616555bad54e4e87f1a399d
Dawit-SEI <DawitTekleab4@gmail.com>
2026-04-21 12:42:55 -0500

    Add files via upload

---
+++ environment/collections-data.py
@@ -0,0 +1,34 @@
+myFiruts = ["apple", "banana", "cherry"]
+print(myFiruts)
+print(type(myFiruts))
+
+print(myFiruts[0])
+print(myFiruts[1])
+print(myFiruts[2])
+
+#Changing the values in the list
+
+myFiruts[2] = "orange"
+print(myFiruts)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+#accessing Dictionary
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
